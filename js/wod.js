fee7c2ae4ea48212c974665bdd038b17ca47259a
Chris Hunt <chrishunt@github.com>
2014-05-06 07:50:08 -0700

    Load wod from url if present; update url on change

--- js/wod.js
+++ js/wod.js
@@ -1,3 +1,13 @@
+function url(wod) {
+  if (wod) {
+    var id = wod.id;
+    if (wod.name) { id += "-" + clean(wod.name); }
+    location.hash = "#" + encodeURIComponent(id);
+  } else {
+    return decodeURIComponent(location.hash.slice(1)).split("-")[0];
+  }
+}
+
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -58,8 +68,20 @@ function random(wods) {
   return wods[Math.floor(Math.random() * wods.length)];
 }
 
+function fromUrl(wods) {
+  if (!url()) { return; }
+
+  for (var i=0; i<wods.length; i++) {
+    if (clean(wods[i].id) == url()) {
+      return wods[i];
+    }
+  }
+}
+
 $.getJSON("wods.json", function (json) {
-  var wod = random(json.wods);
+  var wod = fromUrl(json.wods) || random(json.wods);
+
+  url(wod);
 
   show("name", wod);
   show("description", wod);
